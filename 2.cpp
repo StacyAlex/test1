311663cef46c1a19178bbfc6f2622ae73c82c4be
StacyAlex <36666162+StacyAlex@users.noreply.github.com>
2018-06-11 23:16:10 +0300

    Update and rename 2 to 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,8 +1,8 @@
-#pragma once
-#include <stdexcept>
+#include <iostream>
 #include <limits>
+#include <stdexcept>
 #include <iterator>
-#include <cstring>
+#include <algorithm>
 
 template <class T>
 class Allocator
@@ -11,35 +11,44 @@ public:
     using value_type = T;
     using pointer = T*;
     using size_type = size_t;
-    
-    void construct(pointer ptr) {
+    using reference = T&;
+    using const_reference = const T&;
+
+    void construct(pointer ptr)
+    {
         ptr = new (ptr) value_type();
     }
-    
-    void construct(pointer ptr, const value_type& val) {
+
+    void construct(pointer ptr, const_reference val)
+    {
         ptr = new (ptr) value_type(val);
     }
-    
-    void construct(pointer ptr, value_type&& val) {
+
+    void construct(pointer ptr, value_type&& val)
+    {
         ptr = new (ptr) value_type(std::move(val));
     }
-    
-    void destroy(pointer ptr) {
+
+    void destroy(pointer ptr)
+    {
         ptr->~value_type();
     }
 
-    pointer allocate(size_type count) {
+    pointer allocate(size_type count)
+    {
         if (count > max_size())
             throw std::length_error("Too big count");
-        pointer ptr = (pointer) malloc(sizeof(value_type) * count);
+        auto ptr = (pointer) malloc(sizeof(value_type) * count);
         return ptr;
     }
-    
-    void deallocate(pointer ptr, size_type count) {
+
+    void deallocate(pointer ptr, size_type count)
+    {
         free(ptr);
     }
 
-    size_t max_size() const noexcept {
+    size_t max_size() const noexcept
+    {
         return std::numeric_limits<size_type>::max();
     }
 };
@@ -51,35 +60,53 @@ class Iterator : public std::iterator<std::random_access_iterator_tag, T>
 public:
     using reference = T&;
 
-    explicit Iterator(T* ptr): ptr_(ptr) {}
+    explicit Iterator(T* ptr) : ptr_(ptr) {}
 
-    bool operator==(const Iterator<T>& other) const {
+    bool operator==(const Iterator<T>& other) const
+    {
         return ptr_ == other.ptr_;
     }
 
-    bool operator!=(const Iterator<T>& other) const {
+    bool operator!=(const Iterator<T>& other) const
+    {
         return !(*this == other);
     }
 
-    reference operator*() const {
+    reference operator*() const
+    {
         return *ptr_;
     }
 
-    Iterator& operator++() {
-        ++ptr_;
+    Iterator& operator++()
+    {
+        ptr_++;
         return *this;
     }
-    
-    Iterator& operator--() {
-        --ptr_;
+
+    Iterator operator++(int)
+    {
+        Iterator temp = *this;
+        ++*this;
+        return temp;
+    }
+
+    Iterator& operator--()
+    {
+        ptr_--;
         return *this;
+    }
+
+    Iterator operator--(int)
+    {
+        Iterator temp = *this;
+        --*this;
+        return temp;
     }
 };
 
 template <class T, class Alloc = Allocator<T>>
 class Vector
 {
-public:
     using value_type = T;
     using allocator_type = Alloc;
     using reference = value_type&;
@@ -91,9 +118,18 @@ public:
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = 	std::reverse_iterator<const_iterator>;
     using size_type = size_t;
-    
-    Vector(size_type count = 0) {
-        if (count != 0) {
+
+private:
+    Alloc alloc_;
+    pointer data_ = nullptr;
+    size_type size_ = 0;
+    size_type capacity_ = 0;
+
+public:
+    explicit Vector(size_type count = 0)
+    {
+        if (count != 0)
+        {
             data_ = alloc_.allocate(count);
             for (size_type i = 0; i < count; i++)
                 alloc_.construct(data_ + i);
@@ -101,87 +137,140 @@ public:
             capacity_ = count;
         }
     }
-    
-    reference operator[] (size_type n) {
+
+    reference operator[] (size_type n)
+    {
         if (n >= size_)
             throw std::out_of_range("Out of range");
         return data_[n];
     }
-    
-    const_reference operator[] (size_type n) const {
+
+    const_reference operator[] (size_type n) const
+    {
         if (n >= size_)
             throw std::out_of_range("Out of range");
         return data_[n];
     }
-    
-    bool empty() const noexcept {
+
+    void push_back(const_reference val)
+    {
+        if (size_ == capacity_)
+        {
+            if (size_ == 0)
+                reserve(1);
+            else
+                reserve(2 * size_);
+        }
+        alloc_.construct(data_ + size_, val);
+        size_++;
+    }
+
+    void push_back(value_type&& val)
+    {
+        if (size_ == capacity_)
+        {
+            if (size_ == 0)
+                reserve(1);
+            else
+                reserve(2 * size_);
+        }
+        alloc_.construct(data_ + size_, std::move(val));
+        size_++;
+    }
+
+    void pop_back()
+    {
+        if (size_ > 0)
+            resize(size_ - 1);
+    }
+
+    bool empty() const noexcept
+    {
         return size_ == 0;
     }
-    
-    size_type size() const noexcept {
+
+    size_type size() const noexcept
+    {
         return size_;
     }
-    
-    size_type capacity() const noexcept {
+
+    size_type capacity() const noexcept
+    {
         return capacity_;
     }
-    
-    iterator begin() noexcept {
+
+    iterator begin() noexcept
+    {
         return iterator(data_);
     }
-    
-    const_iterator begin() const noexcept {
+
+    const_iterator begin() const noexcept
+    {
         return const_iterator(data_);
     }
-    
-    iterator end() noexcept {
+
+    iterator end() noexcept
+    {
         return iterator(data_ + size_);
     }
-    
-    const_iterator end() const noexcept {
+
+    const_iterator end() const noexcept
+    {
         return const_iterator(data_ + size_);
     }
-    
-    reverse_iterator rbegin() noexcept {
+
+    reverse_iterator rbegin() noexcept
+    {
         return reverse_iterator(end());
     }
-    
-    const_reverse_iterator rbegin() const noexcept {
+
+    const_reverse_iterator rbegin() const noexcept
+    {
         return const_reverse_iterator(end());
     }
-    
-    reverse_iterator rend() noexcept {
+
+    reverse_iterator rend() noexcept
+    {
         return reverse_iterator(begin());
     }
-    
-    const_reverse_iterator rend() const noexcept {
+
+    const_reverse_iterator rend() const noexcept
+    {
         return const_reverse_iterator(begin());
     }
-    
-    void reserve(size_type n) {
-        if (n > capacity_) {
+
+    void reserve(size_type n)
+    {
+        if (n > capacity_)
+        {
             pointer newData = alloc_.allocate(n);
-            for (size_type i = 0; i < size_; i++) {
+            for (size_type i = 0; i < size_; i++)
+            {
                 alloc_.construct(newData + i, data_[i]);
                 alloc_.destroy(data_ + i);
             }
             std::swap(data_, newData);
             alloc_.deallocate(newData, size_);
-            
+
             capacity_ = n;
         }
     }
-    
-    void resize(size_type n) {
-        if (n < size_) {
+
+    void resize(size_type n)
+    {
+        if (n < size_)
+        {
             for (size_type i = n; i < size_; i++)
                 alloc_.destroy(data_ + i);
         }
-        if (n > capacity_) {
+
+        if(n > capacity_)
             reserve(n);
-        }
-        if (n > size_) {
-            for (size_type i = size_; i < n; ++i) {
+
+        if(n > size_)
+        {
+            for (size_type i = size_; i < n; i++)
+            {
                 alloc_.construct(data_ + i);
             }
         }
@@ -191,42 +280,9 @@ public:
     void clear() noexcept {
         resize(0);
     }
-    
+
     ~Vector() {
         clear();
         alloc_.deallocate(data_, 0);
     }
-    
-    void push_back (const value_type& val) {
-        if (size_ == capacity_) {
-            if (size_ == 0)
-                reserve(1);
-            else
-                reserve(2 * size_);
-        }
-        alloc_.construct(data_ + size_, val);
-        ++size_;
-        
-    }
-    
-    void push_back (value_type&& val) {
-        if (size_ == capacity_) {
-            if (size_ == 0)
-                reserve(1);
-            else
-                reserve(2 * size_);
-        }
-        alloc_.construct(data_ + size_, std::move(val));
-        ++size_;
-    }
-    
-    void pop_back() {
-        if (size_ > 0)
-            resize(size_ - 1);
-    }
-private:
-    Alloc alloc_;
-    pointer data_ = nullptr;
-    size_type size_ = 0;
-    size_type capacity_ = 0;
 };
